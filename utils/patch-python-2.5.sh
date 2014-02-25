ad554c828a1761a82d391981147b55730c67c9f5
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-02-25 14:47:26 +0100

    Two small utilities were added.
    utils/patch-python-2.5.sh
    utils/unpatch-python-2.5.sh
    
    These scripts patch the python scripts to use "python2.5" instead of "python"
    as python interpreter. This is needed to install the scripts on our old
    development host "aragon".

---
+++ utils/patch-python-2.5.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+for f in bin/*; do
+        sed -i '1 s/python\>/python2.5/' $f
+done
---
+++ utils/unpatch-python-2.5.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+for f in bin/*; do
+        sed -i '1 s/python2.5\>/python/' $f
+done
