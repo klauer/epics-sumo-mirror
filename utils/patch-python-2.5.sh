0eed2832611b038f8ead10d14a05c9369eb18c5f
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-03-06 11:38:52 +0100

    The utility scripts in "utils" were no longer needed and are removed.
    These were intended to patch the call to the python interpreter

--- utils/patch-python-2.5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for f in bin/*; do
-        sed -i '1 s/python\>/python2.5/' $f
-done
--- utils/unpatch-python-2.5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for f in bin/*; do
-        sed -i '1 s/python2.5\>/python/' $f
-done
