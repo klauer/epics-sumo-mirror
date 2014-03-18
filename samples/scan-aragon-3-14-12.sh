3c44ee10e43ec4a5fe2d8eaa444b94224977fe10
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-03-18 15:49:11 +0100

    Bugfix: an error in samples/scan-aragon-3-14-12-sh was fixed.
    The call of sumo-db in this shell script used the wrong sumo-db configuration
    file.

--- samples/scan-aragon-3-14-12.sh
+++ samples/scan-aragon-3-14-12.sh
@@ -6,6 +6,6 @@ DBFILE=R3-14-12.DEPS
 
 sumo-scan -c sumo-scan-3-14-12.config all > $SCANFILE
 
-sumo-db -c sumo-db-3-14-8.config convert stable $SCANFILE > $DBFILE
+sumo-db -c sumo-db-3-14-12.config convert stable $SCANFILE > $DBFILE
 
 cp $DBFILE DEPS.DB
