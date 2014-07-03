f37621809834d79e874782162c5dd8e1f498c1dd
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-07-03 10:02:50 +0200

    Bugfix in the module search algorithm (sumo-build commands "try" and "new").
    For commands "new" and "try" the program gets a module specification, a list of
    unversioned and versioned modules, and creates a complete set of versioned
    modules from there.
    
    This set is initially empty and is filled by the algorithm with versioned
    modules. The program takes the first module of the module specification, adds
    all direct and indirect dependencies to the set, and continues with the next
    module specification.
    
    Bofore this patch, the program used what looks like the newest version, for
    each dependency. This causes problems if somewhere in the module specifications
    that follow another version of that dependency is required.
    
    Now the program always looks first at all module specifications if it needs to
    determine which version of a dependency it should use.

---
+++ test/079-sumo-build-try2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ME=`basename $0 .sh`
+
+if [ "$1" = "deps" ]; then
+        echo "$ME.tst: $ME.sh $ME.out $ME.ok"
+        echo
+        exit
+fi
+
+PYTHON=$1
+
+echo -e "\n-> Test sumo-build try" >&2
+
+$PYTHON ../bin/sumo-build --arch vxWorks-68040 --arch vxWorks-ppc603 --db samples/DB --maxstate stable --builddb BUILDS try ALARM:R3-9 BSPDEP_CPUBOARDINIT BSPDEP_TIMER VXSTATS CSM EK MCAN ASYN GENSUB SEQ BASE:TAGLESS-3-14-12-2-1
+
+
+
+
+
