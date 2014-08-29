776bf6481cbba2805ed3f0a4d64ba231f2ab959e
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-08-29 11:26:02 +0200

    Bugfix in sumo-db: cloneversion command didn't always work.
    When cloneversion was used to clone a version that has as source specification
    a path and as new specification a darcs source, the program did exit with an
    exception.
    
    This patch fixes this and modifies a test in order to test this functionality
    in the future.

--- test/045-sumo-db-cloneversion.sh
+++ test/045-sumo-db-cloneversion.sh
@@ -15,5 +15,6 @@ echo -e "\n-> Test sumo-db cloneversion." >&2
 cp samples/IDCP-DEPS.DB $ME-DEPS.tmp
 
 $PYTHON ../bin/sumo-db --db $ME-DEPS.tmp cloneversion ALARM R3-8 R3-4 darcs '*' R3-4
+$PYTHON ../bin/sumo-db --db $ME-DEPS.tmp cloneversion ASYN PATH-4-18 R4-19 darcs 'rcsadm@aragon.acc.bessy.de:/opt/repositories/controls/darcs/epics/support/asyn/4-19' R4-19
 cat $ME-DEPS.tmp 
 
