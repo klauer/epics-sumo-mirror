077e14af7e49aa317b20d4eb1696ab1f8ff2ba36
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-11-18 14:52:40 +0100

    The version was changed to 2.1.1.

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ show_authors= True
 # built documents.
 #
 # The short X.Y version.
-version = "2.1" #VERSION#
+version = "2.1.1" #VERSION#
 
 # The full version, including alpha/beta/rc tags.
 release = version
--- sumolib/JSON.py
+++ sumolib/JSON.py
@@ -16,7 +16,7 @@ import pprint
 import os.path
 import sumolib.lock
 
-__version__="2.1" #VERSION#
+__version__="2.1.1" #VERSION#
 
 assert __version__==sumolib.lock.__version__
 
