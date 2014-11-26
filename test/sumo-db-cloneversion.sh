4ba6a315952d28083742310fb1a837433ff0b91c
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-11-26 10:38:50 +0100

    Tests now work even if lockfile.py is missing.

--- test/sumo-db-cloneversion.sh
+++ test/sumo-db-cloneversion.sh
@@ -21,5 +21,5 @@ echo -e "\n-> Test sumo db cloneversion with wrong sourcespec." >&2
 cp tmp-sumo-db-convert/DEPS.DB $ME-DEPS.tmp
 
 # this command is inteded to fail with "invalid sourcespec":
-$PYTHON ../bin/sumo db --db $ME-DEPS.tmp cloneversion ALARM R3-8-modified R3-4 '*' '*' R3-4 2>&1 || true
+$PYTHON ../bin/sumo db --db $ME-DEPS.tmp cloneversion ALARM R3-8-modified R3-4 '*' '*' R3-4 2>&1 | grep -v "'lockfile' not found"|| true
 
