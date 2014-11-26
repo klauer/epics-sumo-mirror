5acdc02586f70a9d3845dcd6e6c4f142c0e81be5
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2014-11-26 10:38:49 +0100

    The test directory was reorganized.
    The numbering of test scripts has been removed. The new file test/TESTS now
    defines the order of the tests.

---
+++ test/sumo-build-delete.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+ME=`basename $0 .sh`
+
+if [ "$1" = "deps" ]; then
+        echo "$ME.tst: $ME.sh $ME.out $ME.ok sumo-db-convert.tst sumo-build-new.tst"
+        echo
+        exit
+fi
+
+if [ -z "$1" ]; then
+        PYTHON="python"
+else
+        PYTHON=$1
+fi
+
+PWD_NICE=`pwd`
+
+echo -e "\n-> Test sumo build delete" >&2
+
+DEPS=tmp-sumo-db-convert/DEPS.DB
+
+TESTDIR=tmp-sumo-build-new-2
+MYTESTDIR=tmp-$ME
+
+if [ -e $MYTESTDIR ]; then
+    rm -rf $MYTESTDIR
+fi
+
+echo -e "\tcopy $TESTDIR to $MYTESTDIR..." >&2
+
+cp -a $TESTDIR $MYTESTDIR
+
+cd $MYTESTDIR > /dev/null
+
+for f in `find . -name RELEASE`; do
+    sed -i $f -e "s#$TESTDIR#$MYTESTDIR#g"
+done
+
+rm -f *.tmp
+rm -f *.bak
+
+echo -e "\ndirectory tree:"
+echo "> ls $MYTESTDIR:"
+ls  
+echo -e "\nbuild directories:"
+find . -name '*+*'
+
+echo -e "\ncontents of BUILDS file:"
+cat BUILDS.DB
+
+echo -e "\ndelete build 'MYAPP-002'"
+$PYTHON ../../bin/sumo build --db DB --builddb BUILDS.DB delete MYAPP-002
+
+echo -e "\nbuild directories:"
+find . -name '*+*'
+
+echo -e "\ncontents of BUILDS file:"
+cat BUILDS.DB
+
+echo -e "\ndelete build 'MYAPP-001'"
+$PYTHON ../../bin/sumo build --db DB --builddb BUILDS.DB delete MYAPP-001
+
+echo -e "\nbuild directories:"
+find . -name '*+*'
+
+echo -e "\ncontents of BUILDS file:"
+cat BUILDS.DB 
+
---
+++ test/sumo-db-shownewest.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ME=`basename $0 .sh`
+
+if [ "$1" = "deps" ]; then
+        echo "$ME.tst: $ME.sh $ME.out $ME.ok sumo-db-convert.tst"
+        echo
+        exit
+fi
+
+if [ -z "$1" ]; then
+        PYTHON="python"
+else
+        PYTHON=$1
+fi
+
+PWD_NICE=`pwd`
+
+echo -e "\n-> Test sumo db shownewest." >&2
+
+DEPS=tmp-sumo-db-convert/DEPS.DB
+
+$PYTHON ../bin/sumo db --arch vxWorks-68040 --arch vxWorks-ppc603 --db $DEPS shownewest 
+
+
