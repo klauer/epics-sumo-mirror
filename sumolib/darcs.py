0b87927cdcdf40c07ce24c505b842fa6168ac598
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2015-01-23 09:45:24 +0100

    The version was changed to 2.4.

--- sumolib/darcs.py
+++ sumolib/darcs.py
@@ -9,7 +9,7 @@ import os.path
 import sumolib.system
 import os
 
-__version__="2.3.1" #VERSION#
+__version__="2.4" #VERSION#
 
 assert __version__==sumolib.system.__version__
 
--- sumolib/utils.py
+++ sumolib/utils.py
@@ -12,7 +12,7 @@ import os
 import os.path
 import re
 
-__version__="2.3.1" #VERSION#
+__version__="2.4" #VERSION#
 
 _pyver= (sys.version_info[0], sys.version_info[1])
 
