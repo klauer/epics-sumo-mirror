819ae59dd049af2ecfb5f944f80902914cb5262d
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2015-02-19 09:27:38 +0100

    Small changes in the testcode for "sumo build new".
    Up to now, the tests were not run a second time unless "make clean" was called
    before. Now they are always re-run.

--- test/sumo-build-new.sh
+++ test/sumo-build-new.sh
@@ -26,24 +26,20 @@ DEPS=tmp-sumo-db-convert/DEPS.DB
 
 TESTDIR=tmp-$ME
 
-if [ ! -d $TESTDIR ]; then
-    mkdir $TESTDIR
-    cp $DEPS $TESTDIR/DEPS.DB
+rm -rf $TESTDIR
+mkdir $TESTDIR
+cd $TESTDIR > /dev/null
 
-    cd $TESTDIR > /dev/null
+cp ../$DEPS .
 
-    $SUMO --dbdir . --builddir . config make sumo.config
+$SUMO --dbdir . --builddir . config make sumo.config
 
-    $SUMO -c sumo.config build  --buildtag-stem BASE --no-make new BASE:R3-14-12-2-1 1>&2 
-    $SUMO -c sumo.config build  state BASE-001 stable 1>&2 
-    $SUMO -c sumo.config build  --buildtag DISABLED --no-make new BASE:R3-14-12-2-1 BSPDEP_TIMER:R6-2 1>&2 
-    $SUMO -c sumo.config build  state DISABLED disabled 1>&2 
-    $SUMO -c sumo.config build  --buildtag-stem MYAPP --no-make new BASE:R3-14-12-2-1 ALARM:R3-8-modified MCAN:R2-6-3-gp BSPDEP_TIMER:R6-2 BSPDEP_VMETAS:TAGLESS-2-1-modified CSM:TAR-4-1 MISC_DBC:PATH-3-0 MISC_DEBUGMSG:R3-0 SOFT_DEVHWCLIENT:TAR-3-0 1>&2 
-    $SUMO -c sumo.config build  state MYAPP-001 testing 1>&2 
-else
-    echo -e "\t$TESTDIR already exists, effectively skipping this test..." 1>&2
-    cd $TESTDIR > /dev/null
-fi
+$SUMO -c sumo.config build  --buildtag-stem BASE --no-make new BASE:R3-14-12-2-1 1>&2 
+$SUMO -c sumo.config build  state BASE-001 stable 1>&2 
+$SUMO -c sumo.config build  --buildtag DISABLED --no-make new BASE:R3-14-12-2-1 BSPDEP_TIMER:R6-2 1>&2 
+$SUMO -c sumo.config build  state DISABLED disabled 1>&2 
+$SUMO -c sumo.config build  --buildtag-stem MYAPP --no-make new BASE:R3-14-12-2-1 ALARM:R3-8-modified MCAN:R2-6-3-gp BSPDEP_TIMER:R6-2 BSPDEP_VMETAS:TAGLESS-2-1-modified CSM:TAR-4-1 MISC_DBC:PATH-3-0 MISC_DEBUGMSG:R3-0 SOFT_DEVHWCLIENT:TAR-3-0 1>&2 
+$SUMO -c sumo.config build  state MYAPP-001 testing 1>&2 
 
 echo -e "\ndirectory tree (without darcs, maxdepth 2)"
 find . -maxdepth 3 | grep -v '_darcs\|\.hg\(\|ignore\)\|\.git\(\|ignore\)\|\.tmp\|\.bak\|\.coverage'
