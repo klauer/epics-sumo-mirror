b6292e24538eef6e64129579c7b5431bcc3d104c
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2015-05-07 14:23:55 +0200

    "sumo build use" now allows an incomplete module specification.
    The module specification may be incomplete when:
    
    * a build to use is defined by it's build tag
    * a build tag is not given but only one build contains the required modules.
    
    If a build tag is not given and more than one build contains the required
    modules, the program lists all matching builds and exits.

--- test/sumo-build-use-3.sh
+++ test/sumo-build-use-3.sh
@@ -19,7 +19,10 @@ cd $TESTDIR > /dev/null
 echo -e "\nsumo build use, incomplete with buildtag:"
 $SUMO build --dbdir . --builddir . use -t MYAPP-002 MCAN:R2-6-3-gp -o - | sed -e "s#`pwd -P`##"
 
-echo -e "\nsumo build use, incomplete without buildtag:"
+echo -e "\nsumo build use, incomplete without buildtag but unique:"
+$SUMO build --dbdir . --builddir . use MCAN:R2-6-3-gp ALARM:R3-7 -o - 2>&1 | sed -e "s#`pwd -P`##"
+
+echo -e "\nsumo build use, incomplete without buildtag, not unique:"
 $SUMO build --dbdir . --builddir . use MCAN:R2-6-3-gp -o - 2>&1 | sed -e "s#`pwd -P`##"
 
 
