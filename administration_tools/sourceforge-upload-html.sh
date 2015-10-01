d9acab4032a5ea63bd0d034e7917098f89f1e665
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2015-10-01 11:16:25 +0200

    Bugfix: The upload of the documentation to sourceforge didn't work.

--- administration_tools/sourceforge-upload-html.sh
+++ administration_tools/sourceforge-upload-html.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ../doc/_build/html
-echo "put ." | sftp -b - -r goetzpf@web.sourceforge.net:/home/project-web/epics-sumo/htdocs
+echo "put *" | sftp -b - -r goetzpf@web.sourceforge.net:/home/project-web/epics-sumo/htdocs
