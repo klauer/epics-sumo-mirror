1d43dbe58851e2a6e78809e5ec9e3c646df00ba7
Goetz Pfeiffer <goetzpf@googlemail.com>
2018-03-03 10:35:58 +0100

    setup.py was updated and now contains project classifiers.
    These classifiers are used by pypi to categorize an application.

--- setup.py
+++ setup.py
@@ -5,9 +5,18 @@ setup.py file for sumo.
 See http://docs.python.org/install
 on how to use setup.py
 """
-# pylint: disable=invalid-name
-
-my_version="3.2.1" #VERSION#
+# see : http://stackoverflow.com/questions/25337706/setuptools-vs-distutils-why-is-distutils-still-a-thing
+# maybe use this in the future:
+#
+# if len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
+#         sys.argv[1] in ('--help-commands', 'egg_info', '--version',
+#                         'clean')):
+#     # Use setuptools for these commands (they don't work well or at all
+#     # with distutils).  For normal builds use distutils.
+#     try:
+#         from setuptools import setup
+#     except ImportError:
+#         from distutils.core import setup
 
 from distutils.core import setup
 
@@ -18,12 +27,16 @@ import glob
 import subprocess
 import sys
 
+# pylint: disable=invalid-name
+
 if sys.version_info[0] == 2:
     base_name= 'python'
     base_dir = 'python2'
 elif sys.version_info[0] == 3:
     base_name= 'python3'
     base_dir = 'python3'
+
+__version__="3.2" #VERSION#
 
 # utilities -------------------------
 
@@ -82,8 +95,8 @@ def path_rebase(path, base):
     base_l= pathsplit(base)
     if len(path_l)<len(base_l):
         return path
-    for i in range(len(base_l)):
-        if base_l[i]!=path_l[i]:
+    for (i, base_elm) in enumerate(base_l):
+        if base_elm!=path_l[i]:
             return path
     if len(path_l)==len(base_l):
         return ""
@@ -109,12 +122,12 @@ def data_statements(install_path, source_path):
 
 # main      -------------------------
 
-doc_install_dir= os.path.join("doc","sumo-%s" % my_version)
+doc_install_dir= os.path.join("share", "doc",
+                              "sumo-%s" % __version__)
+
 html_install_dir= os.path.join(doc_install_dir, "html")
 
-doc_dir= "doc"
-
-html_build_dir= os.path.join(doc_dir,"_build","html")
+html_build_dir= os.path.join("doc","_build","html")
 
 ## create HTML documentation if it doesn't already exist:
 if not os.path.exists(html_build_dir):
@@ -127,19 +140,51 @@ data_files_list= [(doc_install_dir, ["README", "LICENSE"])]
 data_files_list.extend(data_statements(html_install_dir, html_build_dir))
 
 name='epics-sumo'
-#if ("bdist_rpm" in sys.argv) or ("bdist_deb" in sys.argv):
 if "bdist_rpm" in sys.argv:
     name= base_name+"-"+name
 
 setup(name=name,
-      version= my_version,
+      version= __version__,
       description='Python support tools for EPICS software development',
       author='Goetz Pfeiffer',
       author_email='Goetz.Pfeiffer@helmholtz-berlin.de',
       url='https://goetzpf.bitbucket.io/sumo',
+      download_url='https://bitbucket.org/goetzpf/epics-sumo/downloads',
+
+      classifiers=[
+          'Development Status :: 6 - Mature',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Natural Language :: English',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Utilities',
+          ],
+
       packages=['sumolib'],
       package_dir= {'sumolib': os.path.join(base_dir, "sumolib")},
       #package_data={'sumo': ['data/*']},
+      # the data_files parameter is especially needed for the
+      # rpm file generation:
       data_files= data_files_list,
       license= "GPLv3",
       scripts=[os.path.join(base_dir,p) \
