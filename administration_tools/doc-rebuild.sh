05adae4efec0f6e3cf2bf3037f5a07207bfd3e91
Goetz Pfeiffer <goetzpf@googlemail.com>
2018-03-03 10:35:57 +0100

    Parts of sumo's internal help are now created from rst documentation.
    The script doc-rebuild.sh creates a python module that contains documentation
    for sumo's commands. This module is imported by the sumo script.

--- administration_tools/doc-rebuild.sh
+++ administration_tools/doc-rebuild.sh
@@ -1,9 +1,46 @@
 #!/bin/sh
 
-MYDIR=`dirname "$0"`
+ME=$(readlink -f "$0")
+MYDIR=`dirname "$ME"`
 
 cd "$MYDIR/../doc"
 
 ./make-png.sh
 make clean -s
 make html
+
+cd "$MYDIR/.."
+
+PY_MODULE="sumo_doc.py"
+PY2_FILE="python2/sumolib/$PY_MODULE"
+PY3_FILE="python3/sumolib/$PY_MODULE"
+
+echo "creating $PY2_FILE"
+$MYDIR/rst-extract.py --varname commands -o $PY2_FILE \
+        -f doc/reference-sumo.rst \
+        "maincommands:+" \
+        'subcommands for maincommand "config":+' \
+        'subcommands for maincommand "db":+' \
+        'subcommands for maincommand "build":+'
+$MYDIR/rst-extract.py --no-subchapters --varname completion \
+        -f doc/reference-sumo.rst \
+        "Command completion:-" >> $PY2_FILE
+$MYDIR/rst-extract.py --no-subchapters --varname pager \
+        -f doc/reference-sumo.rst \
+        "The help pager:-" >> $PY2_FILE
+$MYDIR/rst-extract.py --varname options \
+        -f doc/reference-sumo.rst \
+        "Options:-" >> $PY2_FILE
+echo "creating $PY3_FILE"
+cp -a $PY2_FILE $PY3_FILE
+
+PY_MODULE="configuration_doc.py"
+PY2_FILE="python2/sumolib/$PY_MODULE"
+PY3_FILE="python3/sumolib/$PY_MODULE"
+echo "creating $PY2_FILE"
+$MYDIR/rst-extract.py --no-subchapters --varname text -o $PY2_FILE \
+        -f doc/configuration-files.rst \
+        "Configuration Files:=" 
+echo "creating $PY3_FILE"
+cp -a $PY2_FILE $PY3_FILE
+
