e698f1c610885bae87c55a3ce388fa443375d06b
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2018-03-05 13:03:09 +0100

    File README was replaced by README.rst.
    README.sourceforge was removed, we now have a single README file.

--- administration_tools/sourceforge-upload-distfiles.sh
+++ administration_tools/sourceforge-upload-distfiles.sh
@@ -10,4 +10,4 @@ DESTPATH=/home/frs/project/epics-sumo
 #scp dist/*.tar.gz $DESTHOST:$DESTPATH
 #scp dist/*.zip $DESTHOST:$DESTPATH
 scp -r dist/* $DESTHOST:$DESTPATH
-scp README.sourceforge $DESTHOST:$DESTPATH
+scp README.rst $DESTHOST:$DESTPATH
