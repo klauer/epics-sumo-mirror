986d0d526dbc0d59c425466e0f7118a382efa4ec
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2019-10-14 09:24:57 +0200

    A test for the modconvert command was added.

---
+++ test/sumo-db-modconvert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ME=`basename $0 .sh`
+
+source settings.sh
+
+# This program uses the scan file hat was created for test sumo-scan-all.
+
+echo -e "\n-> Test sumo db modconvert." >&2
+
+if [ ! -d $EXAMPLEDIR ]; then
+    echo -e "\tcreating directory $EXAMPLEDIR" >&2
+    mkdir $EXAMPLEDIR
+fi
+
+# the following is a trick to remove the "no dependency info" messages
+# from standard error and leave standard out untouched:
+
+set +o posix
+
+$SUMO db modconvert tmp-sumo-scan-all/SCAN SOFT_DEVHWCLIENT SEQ -D "r\"^$PWD_REAL\",''" -D "r\"$PWD_NICE\",''" -U "r\"^$PWD_REAL\",''" -U "r\"$PWD_NICE\",''" 
+
