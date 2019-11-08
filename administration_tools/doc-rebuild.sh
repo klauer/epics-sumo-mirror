56642ffd162a08bd0da074537b1193fc6be0511b
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2019-11-08 11:45:55 +0100

    The test documentation now shows all steps to bootstrap the tests.
    Some extra python libraries have to be created before the tests can be started.
    These steps are now documented in test/README.rst.
    
    For this, the script administration_tools/doc-rebuild.sh was splitted. The new
    script mk-sumo-doc.sh creates the python libaries for the online documentation
    and is called by doc-rebuild.sh.

--- administration_tools/doc-rebuild.sh
+++ administration_tools/doc-rebuild.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ME=$(readlink -f "$0")
 MYDIR=`dirname "$ME"`
@@ -9,38 +9,4 @@ cd "$MYDIR/../doc"
 make clean -s
 make html
 
-cd "$MYDIR/.."
-
-PY_MODULE="sumo_doc.py"
-PY2_FILE="python2/sumolib/$PY_MODULE"
-PY3_FILE="python3/sumolib/$PY_MODULE"
-
-echo "creating $PY2_FILE"
-$MYDIR/rst-extract.py --varname commands -o $PY2_FILE \
-        -f doc/reference-sumo.rst \
-        "maincommands:+" \
-        'subcommands for maincommand "config":+' \
-        'subcommands for maincommand "db":+' \
-        'subcommands for maincommand "build":+'
-$MYDIR/rst-extract.py --no-subchapters --varname completion \
-        -f doc/reference-sumo.rst \
-        "Command completion:-" >> $PY2_FILE
-$MYDIR/rst-extract.py --no-subchapters --varname pager \
-        -f doc/reference-sumo.rst \
-        "The help pager:-" >> $PY2_FILE
-$MYDIR/rst-extract.py --varname options \
-        -f doc/reference-sumo.rst \
-        "Options:-" >> $PY2_FILE
-echo "creating $PY3_FILE"
-cp -a $PY2_FILE $PY3_FILE
-
-PY_MODULE="configuration_doc.py"
-PY2_FILE="python2/sumolib/$PY_MODULE"
-PY3_FILE="python3/sumolib/$PY_MODULE"
-echo "creating $PY2_FILE"
-$MYDIR/rst-extract.py --no-subchapters --varname text -o $PY2_FILE \
-        -f doc/configuration-files.rst \
-        "Configuration Files:=" 
-echo "creating $PY3_FILE"
-cp -a $PY2_FILE $PY3_FILE
-
+$MYDIR/mk-sumo-doc.sh
