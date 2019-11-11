07300609281388c0e9611534f503b2b08ffc88dc
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2019-11-11 16:09:40 +0100

    docker-run.sh now creates a log file.

--- administration_tools/docker-run-all.sh
+++ administration_tools/docker-run-all.sh
@@ -5,8 +5,13 @@ MYDIR=$(dirname "$ME")
 
 cd "$MYDIR"
 
+LOGFILE="DOCKER-RUN.LOG"
+
 images=$(./docker-build.sh | grep '^\(debian\|fedora\)')
 
+rm -f $LOGFILE
+
 for img in $images; do
+    echo "./docker-run.sh $img"
     ./docker-run.sh $img
 done
