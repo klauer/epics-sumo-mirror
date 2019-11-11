c97bed500f4a9d6248c7172273fad4a6c866e4c6
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2019-11-11 16:09:41 +0100

    The version was changed to 3.6.1.

--- python2/sumolib/Builds.py
+++ python2/sumolib/Builds.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 import sumolib.ModuleSpec
 import sumolib.JSON
 
-__version__="3.6" #VERSION#
+__version__="3.6.1" #VERSION#
 
 assert __version__==sumolib.ModuleSpec.__version__
 assert __version__==sumolib.JSON.__version__
--- python3/sumolib/system.py
+++ python3/sumolib/system.py
@@ -7,7 +7,7 @@
 import os
 import subprocess
 
-__version__="3.6" #VERSION#
+__version__="3.6.1" #VERSION#
 
 # -----------------------------------------------
 # basic system utilities
