f74bcb968d208e929324eec0b9bea659ac7eafc4
Goetz Pfeiffer <Goetz.Pfeiffer@helmholtz-berlin.de>
2020-02-21 14:00:51 +0100

    Package building now uses podman instead of docker.

--- administration_tools/docker-run.sh
+++ administration_tools/docker-run.sh
@@ -5,6 +5,8 @@ MYDIR=$(dirname "$ME")
 
 cd "$MYDIR"
 
+source docker.config
+
 LOGFILE="DOCKER-RUN.LOG"
 
 APPLICATION=sumo
@@ -135,4 +137,4 @@ fi
 echo "---------------------------------------" >> $MYDIR/$LOGFILE
 echo "$me $DOCKERFILE" >> $MYDIR/$LOGFILE
 
-CMD "docker run -t --volume $top/$dist_dir:/root/dist --volume $top:/root/$APPLICATION -i $DOCKERIMAGE $PROG" 2>&1 | tee -a $MYDIR/$LOGFILE
+CMD "$DOCKER run -t --volume $top/$dist_dir:/root/dist --volume $top:/root/$APPLICATION -i $DOCKERIMAGE $PROG" 2>&1 | tee -a $MYDIR/$LOGFILE
