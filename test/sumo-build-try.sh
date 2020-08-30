e24ce5fe151e9e89d4f094dd09c8ad50abe38648
Goetz Pfeiffer <goetzpf@googlemail.com>
2020-08-30 15:37:41 +0200

    Test option --exclude-states for command "build try".

--- test/sumo-build-try.sh
+++ test/sumo-build-try.sh
@@ -56,3 +56,11 @@ $SUMO build --dbdir $DEPSDIR --builddir $SUPPORTDIR --detail 2 try BASE:R3-14-12
 echo -e "\n----------------------------"
 echo -e "\ntry with completely versioned modulelist, detail 3:\n"
 $SUMO build --dbdir $DEPSDIR --builddir $SUPPORTDIR --detail 3 try BASE:R3-14-12-2-1 ALARM:R3-8-modified MCAN:TAGLESS-2-6-1 BSPDEP_TIMER:R6-2 BSPDEP_VMETAS:TAGLESS-2-1-modified MISC_DBC:PATH-3-0 MISC_DEBUGMSG:R3-0 SOFT_DEVHWCLIENT:TAR-3-0
+
+# Note: the following test does not really test that "-X stable" works since
+# this doesn' filter builds if there is only one. This just tests that option
+# "-X" is accepted and that a given string is internally compiled to a regular
+# expression.
+echo -e "\n----------------------------"
+echo -e "\ntry with completely versioned modulelist, only stable builds, detail 3:\n"
+$SUMO build --dbdir $DEPSDIR --builddir $SUPPORTDIR --detail 3 -X stable try BASE:R3-14-12-2-1 ALARM:R3-8-modified MCAN:TAGLESS-2-6-1 BSPDEP_TIMER:R6-2 BSPDEP_VMETAS:TAGLESS-2-1-modified MISC_DBC:PATH-3-0 MISC_DEBUGMSG:R3-0 SOFT_DEVHWCLIENT:TAR-3-0
