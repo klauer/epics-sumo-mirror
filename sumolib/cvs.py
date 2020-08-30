a7436f2d977ddfb2fb34bf49e92ab86afdf073f3
Goetz Pfeiffer <goetzpf@googlemail.com>
2020-08-30 15:44:38 +0200

    The version was changed to 4.1.

--- sumolib/cvs.py
+++ sumolib/cvs.py
@@ -12,7 +12,7 @@ import sumolib.system
 import sumolib.utils
 import sumolib.lock
 
-__version__="4.0.2" #VERSION#
+__version__="4.1" #VERSION#
 
 assert __version__==sumolib.system.__version__
 
--- sumolib/system.py
+++ sumolib/system.py
@@ -7,7 +7,7 @@ import os
 import shutil
 import subprocess
 
-__version__="4.0.2" #VERSION#
+__version__="4.1" #VERSION#
 
 # -----------------------------------------------
 # basic system utilities
