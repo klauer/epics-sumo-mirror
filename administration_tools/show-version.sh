d549a6dea0dd4a4f2713523d92c44a9d44ae877d
Goetz Pfeiffer <goetzpf@googlemail.com>
2020-09-10 13:00:02 +0200

    A file RELEASES.rst was added, it documents changes in sumo releases.

--- administration_tools/show-version.sh
+++ administration_tools/show-version.sh
@@ -5,4 +5,17 @@ MYDIR=$(dirname "$ME")
 
 cd "$MYDIR"
 
-PYTHONPATH=.. python3 ../bin/sumo --version
+if [ "$1" = "-h" ]; then
+    echo "$ME : show the current sumo version."
+    echo 
+    echo "options:"
+    echo "-h  : this help"
+    echo "-b  : just show the pure version number"
+    exit 0
+fi
+
+if [ "$1" = "-b" ]; then
+    PYTHONPATH=.. python3 ../bin/sumo --version | sed -e 's/sumo *//'
+else
+    PYTHONPATH=.. python3 ../bin/sumo --version
+fi
